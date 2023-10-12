1cc04f5e3930ff7dace00e56dc11351f93e58633
badgelatte <saucy5939@gmail.com>
2023-10-12 19:12:14 +0900

    StdInNode 작성

---
+++ src/main/java/com/nhnacademy/aiot/node/StdInNode.java
@@ -0,0 +1,57 @@
+package com.nhnacademy.aiot.node;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import com.nhnacademy.aiot.message.Message;
+
+public class StdInNode extends Node implements Inputable {
+    Map<Integer, Target> targets; 
+    Message message;
+    
+    public StdInNode() {
+        targets = new HashMap<>();
+    }
+    
+    /** 
+     * scanner를 message로 형변환합니다.
+     * 
+     * @param scanner 입력을 받습니다.
+     */
+    public void scanToMessage(Scanner scanner) {
+        /**
+         * 유효시간 1초
+         */
+        message = new Message(10000);
+        // message에 scanner넣을때마다 node들한테 뿌리니까 key로 분류할 필요 없다
+        message.append("value", scanner.nextLine());
+    }
+    
+    /**
+     * StdInNode의 port와 target을 연결합니다.
+     * 
+     * @param outputPort StdInNode의 port로, 맵의 Integer로 들어갑니다.
+     * @param target Node의 종류를 말하며, Map의 Target으로 들어갑니다.
+     */
+    @Override
+    public void connect(int outputPort, Target target) {
+        targets.put(outputPort,target);
+    }
+    
+    @Override
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        scanToMessage(scanner);
+       
+        for (Target target : targets.values()) {
+            try{
+                target.add(message);
+            } catch (InterruptedException e) {
+                System.out.println("노드로 message 못 보냄\n" +e);
+            }
+        }
+    }
+
+}
